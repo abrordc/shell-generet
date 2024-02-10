ca5bc1a6e16cd2df80cf57ef5b95b577add48057
Abror <114936122+DemtimCod@users.noreply.github.com>
2024-02-10 15:58:04 +0700

    generate file and crete text data

--- script.sh
+++ script.sh
@@ -5,3 +5,34 @@ mkdir data
 cd data
 echo "generetet script..... "
 touch script.js
+touch index.html
+touch style.css
+touch script.ts
+touch package.yml
+touch package.json
+touch package.lock.composer
+touch Tsconfic.ts
+touch Dockerfile
+touch docker-compos.yml
+touch babel.confic.json
+touch remote.cpp
+touch script.test.js
+touch remote.app
+mkdir node_modules
+cd node_modules
+mkdir react
+mkdir axios
+mkdir express
+mkdir next-js
+mkdir fecth-ap-test
+mkdir hello-api
+cd ..
+mkdir views
+cd views
+touch view.blade.php
+touch view.ejs
+touch view.vue
+touch view.jsx
+touch view.tsx
+echo "import { data } from 'data'" > view.tsx
+echo "script berhasil di buat!!"
